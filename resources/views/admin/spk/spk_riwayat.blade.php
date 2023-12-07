97d7384bdd9ed762159a6a8c7fbf8e39f42d2fcf
dolyded <dolyprahoro@gmail.com>
2023-12-07 14:56:22 +0700

    progres spk

---
+++ resources/views/admin/spk/spk_riwayat.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.main')
+@section('content')
+
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Riwayat SPK</h1>
+    </div>
+
+    <div class="card border-left-dark mb-4">
+        <div class="card-body">
+            <div class="table-responsive card-left-dark text-dark">
+                <table class="table table-bordered table-align-center" width="100%" cellspacing="0">
+                    <thead class="table-secondary text-dark">
+                        <tr>
+                            <th class="col-sm-3">NO SPK</th>
+                            <th class="col-sm-2">Nama Shift</th>
+                            <th class="col-sm-2">Nama Group</th>
+                            <th class="col-sm-3">Tanggal Dibuat</th>
+                            <th class="col-sm-2">Aksi</th>
+                        </tr>
+                    </thead>
+                    <tbody id="table-body-spk">
+                        @foreach ($tspk_header as $item)
+                        <tr>
+                            <td>{{ $item->spk_no }}</td>
+                            <td>{{ $item->nama_shift }}</td>
+                            <td>{{ $item->nama_group }}</td>
+                            <td>{{ \Carbon\Carbon::parse($item->created_date)->format('d/m/Y') }}</td>
+                            <td class="text-center">
+                                <a href="riwayat-spk/update/{{ $item->id_h }}/{{ $item->spk_no }}" class="text-bold">
+                                    <button type="button" class="btn btn-sm btn-outline-info">Revisi</i></button>
+                                </a>
+                                <a href="spk-download/{{ $item->id_h }}/{{ $item->spk_no }}" target="_blank" class="text-bold">
+                                    <button type="button" class="btn btn-sm btn-success"><i class="fa fa-download" aria-hidden="true"></i></button>
+                                </a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+    
+
+@endsection
---
+++ resources/views/admin/spk/spk_update.blade.php
@@ -0,0 +1,207 @@
+@extends('layouts.main')
+@section('content')
+
+<div class="card card-primary card-tabs" width="50%">
+    @foreach ($group_shift as $item)
+        <div class="pt-2 h4 p-3 text-white bg-primary font-weight-bold">Operator {{ $item->nama_group }}</div>
+    @endforeach
+    <div class="card-header">
+        <ul class="nav nav-tabs h5 ml-1" id="custom-tabs-two-tab" role="tablist">
+            <li class="nav-item">
+                <a class="nav-link active" id="custom-tabs-two-cc-tab" data-toggle="pill" href="#custom-tabs-two-cc" role="tab" aria-controls="custom-tabs-two-cc" aria-selected="true">CC</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="custom-tabs-two-rtg-tab" data-toggle="pill" href="#custom-tabs-two-rtg" role="tab" aria-controls="custom-tabs-two-rtg" aria-selected="false">RTG & ARTG</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" id="custom-tabs-two-rs-tab" data-toggle="pill" href="#custom-tabs-two-rs" role="tab" aria-controls="custom-tabs-two-rs" aria-selected="false">RS/SS</a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="card-body">
+        <div class="tab-content" id="custom-tabs-two-tabContent">
+        <div class="tab-pane fade show active" id="custom-tabs-two-cc" role="tabpanel" aria-labelledby="custom-tabs-two-cc-tab">
+            <form action="" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="table-responsive">
+                <table class="table table-bordered table-align-center" cellspacing="0">
+                    <thead class="table-secondary text-center text-dark">
+                        <tr>
+                            <th>ID</th>
+                            <th class="col-sm-3">Nama Operator</th>
+                            <th class="col-sm-2">Jobdesk</th>
+                            <th class="col-sm-5">Pilih Alat</th>
+                            <th class="col-sm-2">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody id="table-body-spk">
+                        @foreach ($operator_cc as $item)
+                        <tr>
+                            <!--  -->
+                            <input type="text" name="id_group" value="{{ $id_group }}" hidden>                            
+                            <input type="text" name="nipp{{$item->id}}" value="{{ $item->nipp }}" hidden>
+                            <td> {{ $item->id }} </td>
+                            <td> <input type="text" name="nama{{$item->id}}" value="{{ $item->nama }}" hidden> {{ $item->nama }}</td>
+                            <td> <input type="text" name="jobdesk{{$item->id}}" value="{{ $item->jobdesk }}" hidden > {{ $item->jobdesk }}</td>
+                            <td> 
+                                <select class="js-example-basic-multiple-cc" name="tambah_alat_cc_{{$item->id}}[]" multiple="multiple">
+                                    @foreach ($alat_cc as $alat)    
+                                        <option <?php if($item->jobdesk == $alat->text) { echo 'selected'; } ?> value="{{ $alat->id }}" >{{ $alat->text }}</option>
+                                    @endforeach
+                                </select>
+                            </td>
+                            <td>
+                                <select class="form-control form-control" aria-label="Default select example" name="status_absen{{$item->id}}" id="jenis_absen">
+                                    @foreach ($jenis_absen as $item)
+                                    <option value="{{ $item->nama }}">{{ $item->nama }}</option>
+                                    @endforeach
+                                </select>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <div class="tab-pane fade" id="custom-tabs-two-rtg" role="tabpanel" aria-labelledby="custom-tabs-two-rtg-tab">
+            <div class="table-responsive">
+                <table class="table table-bordered table-align-center" cellspacing="0">
+                    <thead class="table-secondary text-center text-dark">
+                        <tr>
+                            <th>ID</th>
+                            <th class="col-sm-3">Nama Operator</th>
+                            <th class="col-sm-2">Jobdesk</th>
+                            <th class="col-sm-5">Pilih Alat</th>
+                            <th class="col-sm-2">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody id="table-body-spk">
+                        @foreach ($operator_rtg as $item)
+                        <tr>
+                            
+                            <input type="text" name="id_group" value="{{ $id_group }}" hidden>                            
+                            <input type="text" name="nipp" value="{{ $item->nipp }}" hidden>
+                            <td> {{ $item->id }} </td>
+                            <td> <input type="text" name="nama{{ $item->id}}" value="{{ $item->jobdesk }}" hidden > {{ $item->nama }}</td>
+                            <td> <input type="text" name="jobdesk{{ $item->id}}" value="{{ $item->jobdesk }}" hidden > {{ $item->jobdesk }}</td>
+                            <td> 
+                                <select class="js-example-basic-multiple-rtg" name="tambah_alat_rtg_{{$item->id}}[]" multiple="multiple">
+                                    @foreach ($alat_rtg as $rtg)    
+                                        <option <?php if($item->jobdesk == $rtg->text) { echo 'selected'; } ?> value="{{ $rtg->id }}" >{{ $rtg->text }}</option>
+                                    @endforeach
+                                </select>
+                            </td>
+                            <td>
+                                <select class="form-control form-control" aria-label="Default select example" name="status_absen{{$item->id}}" id="jenis_absen">
+                                    @foreach ($jenis_absen as $item)
+                                    <option value="{{ $item->nama }}">{{ $item->nama }}</option>
+                                    @endforeach
+                                </select>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div> 
+                        
+        </div>
+
+        <div class="tab-pane fade" id="custom-tabs-two-rs" role="tabpanel" aria-labelledby="custom-tabs-two-rs-tab">
+            <div class="table-responsive">
+                <table class="table table-bordered table-align-center" cellspacing="0">
+                    <thead class="table-secondary text-center text-dark">
+                        <tr>
+                            <th>ID</th>
+                            <th class="col-sm-3">Nama Operator</th>
+                            <th class="col-sm-2">Jobdesk</th>
+                            <th class="col-sm-5">Pilih Alat</th>
+                            <th class="col-sm-2">Status</th>
+                        </tr>
+                    </thead>
+                    <tbody id="table-body-spk">
+                        @foreach ($operator_rs as $item)
+                        <tr>
+                           
+                            <input type="text" name="id_group" value="{{ $id_group }}" hidden>                            
+                            <input type="text" name="nipp" value="{{ $item->nipp }}" hidden>
+                            <td> {{ $item->id }} </td>
+                            <td> <input type="text" name="nama{{ $item->id}}" value="{{ $item->jobdesk }}" hidden > {{ $item->nama }}</td>
+                            <td> <input type="text" name="jobdesk{{ $item->id}}" value="{{ $item->jobdesk }}" hidden > {{ $item->jobdesk }}</td>
+                            <td> 
+                                <select class="js-example-basic-multiple-rtg" name="tambah_alat_rs_{{$item->id}}[]" multiple="multiple">
+                                    @foreach ($alat_rs as $rs)    
+                                        <option <?php if($item->jobdesk == $rs->text) { echo 'selected'; } ?> value="{{ $rs->id }}" >{{ $rs->text }}</option>
+                                    @endforeach
+                                </select>
+                            </td>
+                            <td>
+                                <select class="form-control form-control" aria-label="Default select example" name="status_absen{{$item->id}}" id="jenis_absen">
+                                    @foreach ($jenis_absen as $item)
+                                    <option value="{{ $item->nama }}">{{ $item->nama }}</option>
+                                    @endforeach
+                                </select>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>  
+            <button type="submit" class="btn btn-success float-right mb-4">Submit</button>
+            </form>         
+        </div>
+    </div>
+    <button type="submit" class="btn btn-outline-primary mb-4" id="btnSelanjutnya">
+        Selanjutnya <i class="pr-2 fas fa-arrow-right"></i> 
+    </button>
+</div>
+</div>
+
+<script>
+    window.onload = function() {
+        $(document).ready(function() {
+            $('.js-example-basic-multiple-cc').select2({
+                theme: "bootstrap-5",
+                placeholder: "Pilih Alat CC",
+                width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style'
+            });
+        });
+
+        $(document).ready(function() {
+            $('.js-example-basic-multiple-rtg').select2({
+                theme: "bootstrap-5",
+                placeholder: "Pilih Alat RTG",
+                width: $(this).data('width') ? $(this).data('width') : $(this).hasClass('w-100') ? '100%' : 'style'
+            });
+        });
+
+        
+        // $(document).ready(function () {
+            // Tambahkan event listener pada tombol "Selanjutnya"
+            // $('#btnSelanjutnya').on('click', function () {
+                // Pindahkan fokus ke tab RTG setelah mengklik tombol
+        //         $('#custom-tabs-two-rtg-tab').tab('show');
+        //     });
+        // });
+
+        $(document).ready(function () {
+        // Tambahkan event listener pada tombol "Selanjutnya"
+        $('#btnSelanjutnya').on('click', function () {
+            // Periksa tab yang aktif saat ini
+            var currentTab = $('.nav-tabs .active');
+
+            // Periksa id tab yang aktif saat ini
+            if (currentTab.attr('id') === 'custom-tabs-two-cc-tab') {
+                // Pindahkan fokus ke tab RTG jika sekarang berada di tab CC
+                $('#custom-tabs-two-rtg-tab').tab('show');
+            } else if (currentTab.attr('id') === 'custom-tabs-two-rtg-tab') {
+                // Pindahkan fokus ke tab RS jika sekarang berada di tab RTG
+                $('#custom-tabs-two-rs-tab').tab('show');
+            }
+        });
+    });
+    }
+</script>
+
+@endsection
